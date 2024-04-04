eaa1f62aa4196834b60da4da0b90c7874cc71f7b
Ivan Orlov <iorlov@dnomads.pro>
2024-04-04 18:15:18 +0700

    set layout app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App)
+  .use(vuetify)
+  //.use(router)
+  .mount('#app')
